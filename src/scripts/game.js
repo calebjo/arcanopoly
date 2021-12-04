7ade3231d6f2ebe5ef35a47f8a352a263244b67c
calebjo <caleb.ri.jones@gmail.com>
2021-12-04 16:13:46 -0500

    Add methods in game.js for player position

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -24,22 +24,42 @@ export class Game {
     onGameStart(){
         // when Start is pressed, place the player tokens in the tavern
 
+        const playerTokens = document.getElementById('player-tokens')
+        for (let i = 0; i < this.players.length; i++){
+            let player = document.createElement('div')
+            player.classList.add('player-token')
+            player.setAttribute('id', `player-${i}`)
+            let playerIcon = document.createElement('img')
+            // Create a new token element based on the given player's sprite
+            playerIcon.setAttribute('src', `${this.players[i].sprite}`)
+
+            // Set each player token position to the tavern (sq-0)
+            this.movePlayer(player, 'sq-0')
+
+            // attach token to the DOM
+            player.appendChild(playerIcon)
+            // playerTokens.appendChild(player)
+            document.getElementById('sq-0').appendChild(player)
+        }
+        
+        // remove bouncing animation for start turn button
+        this.mainButton.style.removeProperty('animation')
 
         // then play a turn of the game
         console.log('The game has started!')
-        this.playTurn();
+        this.playTurn()
     }
 
     playTurn(){
         console.log(`${this.currentPlayer.name} is playing a turn!`) // TEST
-        this.turnNum += 1;
+        this.turnNum += 1
 
         // change center button to 'Roll'
-        this.mainButton.children[0].innerText = 'Roll';
+        this.mainButton.children[0].innerText = 'Roll'
 
         // when 'Roll' is clicked, roll the dice
-        this.mainButton.addEventListener("click", callRoll);
-        const that = this;
+        this.mainButton.addEventListener("click", callRoll)
+        const that = this
         function callRoll(){
             that.diceRoll = that.handleDiceRoll.call(that);
             that.mainButton.children[0].innerText  = 'End'
@@ -77,7 +97,7 @@ export class Game {
             that.currentPlayer = that.players[nextPlayerIdx];
 
             console.log(`The next player will be ${that.currentPlayer.name}.`) // DEBUG
-            console.log('End of the turn.')// TEST
+            console.log('End of the turn.')// DEBUG
 
             // if the game is not already won, play another turn
             if (!that.isWon()){ 
@@ -88,9 +108,22 @@ export class Game {
         }
     }
 
+    // -------------------------------------------------------------------------------
+    // Game logic outside of loop
+
+    movePlayer(playerSprite, target){
+        // takes in a player element (div with token inside) and a target square element
+        // moves the player's token to the target position
+        const playerObject = this.players.find(player => player.sprite === playerSprite)
+        console.log(playerSprite)
+        console.log(playerObject)
+        // const tavernPos = document.getElementById('sq-0').getBoundingClientRect()
+        // console.log(tavernPos) // DEBUG
+    }
+
     handleDiceRoll(){
         // when Roll button is clicked, calculate dice to roll and roll them
-        console.log(`Rolling dice for ${this.currentPlayer.name}!`)
+        console.log(`Rolling dice for ${this.currentPlayer.name}!`) // DEBUG
 
         let diceRoll = 0;
 
@@ -100,7 +133,7 @@ export class Game {
         // roll dice
         diceRoll = this.rollDice(playerDiceNum, playerMax);
 
-        console.log(`The dice roll was a ${diceRoll}!`);
+        console.log(`The dice roll was a ${diceRoll}!`) // DEBUG
         return diceRoll;
     }
 
@@ -132,6 +165,6 @@ export class Game {
 
     gameOver(){
         // PLACEHOLDER
-        console.log('The game is over!')
+        console.log('The game is over!') // DEBUG
     }
 }
